4aa7d5d7ee8abd2a858dcc44d9abbe6b59f94c45
Ivan Maister <ivanmaister7@gmail.com>
2022-11-03 10:19:07 +0200

    Initial Commit

---
+++ RecipeNetworkingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeNetworkingApp
+//
+//  Created by Master on 03.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
