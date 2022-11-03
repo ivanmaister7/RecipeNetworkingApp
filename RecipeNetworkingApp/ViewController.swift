4212f7469bbda91d9671370c9a750513088f008f
Ivan Maister <ivanmaister7@gmail.com>
2022-11-03 15:12:25 +0200

    Add pods files to repo

--- RecipeNetworkingApp/ViewController.swift
+++ RecipeNetworkingApp/ViewController.swift
@@ -8,16 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    
-//    #if DEBUG
-//    let host = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/test"
-//    #else
-//    let host = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/prod"
-//    #endif
-//
-//
-//
     
     override func viewDidLoad() {
         super.viewDidLoad()
